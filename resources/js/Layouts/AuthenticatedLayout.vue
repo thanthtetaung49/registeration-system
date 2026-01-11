41ceda0a6c971393aa3a13b658a8e460317c42fb
Thant Htet Aung <thanthtetaung12@gmail.com>
2026-01-11 22:59:05 +0630

    refactor: Remove commented-out dark mode buttons from AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -91,23 +91,6 @@ const toggle = () => {
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-              <!-- <button type="button" class="hs-dark-mode hs-dark-mode-active:hidden block"
-                data-hs-theme-click-value="dark" v-on:click="toggleDarkMode">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                  stroke="currentColor" class="size-4">
-                  <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                </svg>
-              </button>
-
-              <button type="button" class="hs-dark-mode hs-dark-mode-active:inline-flex hidden dark:text-white"
-                data-hs-theme-click-value="light" v-on:click="toggleDarkMode">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                  stroke="currentColor" class="size-4">
-                  <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                </svg>
-              </button> -->
 
               <button type="button" v-on:click="toggleDarkMode"
                 class="hs-dark-mode-active:hidden block hs-dark-mode font-medium text-gray-800 rounded-full hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 dark:text-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
